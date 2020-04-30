79529915d3bca05b4e3eded0674123154f7e9ccc
Egemen <uzunaliegemen@gmail.com>
2020-04-30 12:42:50 +0200

    removed unused packages

--- src/App/App.js
+++ src/App/App.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
+import SplashScreen from 'react-native-splash-screen';
 import InitialStack from './stacks/InitialStack';
 import TabNavigator from './TabNavigator';
-
-import SplashScreen from 'react-native-splash-screen';
 
 export default function App() {
   SplashScreen.hide(); // hides the splashscreen after bundle load, prevents the flashing splashscreen bug
