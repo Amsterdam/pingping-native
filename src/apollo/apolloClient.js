4b410382ed1b97a83380972dd6e3e166da6ad73e
Egemen <uzunaliegemen@gmail.com>
2020-05-13 13:04:13 +0200

    Added unfetch to make apollo request responses debuggable

--- src/apollo/apolloClient.js
+++ src/apollo/apolloClient.js
@@ -2,6 +2,7 @@ import {ApolloClient, createHttpLink, InMemoryCache, gql} from '@apollo/client';
 import AsyncStorage from '@react-native-community/async-storage';
 import {setContext} from '@apollo/link-context';
 import GET_MODAL_STATE from './Query/getModalState';
+import unfetch from 'unfetch';
 
 const cache = new InMemoryCache();
 
@@ -20,6 +21,7 @@ const authLink = setContext(async (_, {headers}) => {
 
 const httpLink = createHttpLink({
   uri: 'http://localhost:4000',
+  fetch: unfetch,
 });
 
 const client = new ApolloClient({
