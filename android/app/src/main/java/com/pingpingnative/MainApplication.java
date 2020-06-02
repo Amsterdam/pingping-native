dff7a80766cb8fe58a8412a692f84c4171462807
Egemen <uzunaliegemen@gmail.com>
2020-06-02 12:05:35 +0200

    prepare android for remote notifications

--- android/app/src/main/java/com/pingpingnative/MainApplication.java
+++ android/app/src/main/java/com/pingpingnative/MainApplication.java
@@ -11,6 +11,8 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import com.wix.reactnativenotifications.RNNotificationsPackage;
+
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -27,7 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
-          return packages;
+            return packages;
         }
 
         @Override
