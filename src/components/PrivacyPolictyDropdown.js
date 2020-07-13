2454d4ac114ca94c207b2cc8a64756646a7bfa4a
Egemen <uzunaliegemen@gmail.com>
2020-07-13 14:51:03 +0200

    add native version and move faqitems

--- src/components/PrivacyPolictyDropdown.js
+++ src/components/PrivacyPolictyDropdown.js
@@ -3,6 +3,7 @@ import {View, StyleSheet, TouchableWithoutFeedback} from 'react-native';
 import {Icon} from 'native-base';
 import Title from './typography/Title';
 import Body from './typography/Body';
+import {faqItems} from '../lib/faqItems';
 
 const styles = StyleSheet.create({
   policyContainer: {
@@ -42,45 +43,6 @@ const styles = StyleSheet.create({
   faqItemContainer: {marginTop: 5, marginBottom: 5},
   faqFontSize: {fontSize: 12},
 });
-
-const faqItems = [
-  {
-    question:
-      'Welke gegevens van mij gebruiken jullie voor het advies over mijn route?',
-    answer:
-      'Geboortemaand/jaar, adres, bankrekening, DigiD, Zorgverzekering, Zorgtoeslag, Inschrijving Woningnet.',
-  },
-  {
-    question: 'Waarom slaan wij jouw gegevens op?',
-    answer:
-      'Omdat dit makkelijker is voor jou om de app te gebruiken. Dan hoef je niet elke keer alles opnieuw in te vullen.',
-  },
-  {
-    question: 'Waar slaan jullie mijn gegevens op ?',
-    answer:
-      'We slaan jouw gegevens veilig op bij het datapunt van de gemeente Amsterdam. ',
-  },
-  {
-    question: 'Wat gebeurt er verder met die gegevens?',
-    answer:
-      'Helemaal niets. Als je de app niet meer gebruikt wissen we jouw gegevens na 3 maanden.',
-  },
-  {
-    question: 'Kan ik mijn gegevens wijzigen of verwijderen?',
-    answer:
-      'Ja, dat kan (maar soms ook niet). Klik op de link voor meer informatie: amsterdam.nl/privacy',
-  },
-  {
-    question: 'Wie kan ik bellen met vragen over mijn gegevens?',
-    answer:
-      'Bellen gatver ;). Je kunt wel mailen naar Robert Jan Taling, onze TADA inspirator: r.j.taling@amsterdam.nl.',
-  },
-  {
-    question: 'TADA? Wat is dat?',
-    answer:
-      'De gemeente Amsterdam past bij het gebruik van jouw persoonsgegevens 6 principes toe. Klik op de link om daar meer over te weten: tada.city',
-  },
-];
 
 const PrivacyPolicyDropdown = ({open, toggleOpen}) => {
   return (
---
+++ src/lib/faqItems.js
@@ -0,0 +1,38 @@
+export const faqItems = [
+  {
+    question:
+      'Welke gegevens van mij gebruiken jullie voor het advies over mijn route?',
+    answer:
+      'Geboortemaand/jaar, adres, bankrekening, DigiD, Zorgverzekering, Zorgtoeslag, Inschrijving Woningnet.',
+  },
+  {
+    question: 'Waarom slaan wij jouw gegevens op?',
+    answer:
+      'Omdat dit makkelijker is voor jou om de app te gebruiken. Dan hoef je niet elke keer alles opnieuw in te vullen.',
+  },
+  {
+    question: 'Waar slaan jullie mijn gegevens op ?',
+    answer:
+      'We slaan jouw gegevens veilig op bij het datapunt van de gemeente Amsterdam. ',
+  },
+  {
+    question: 'Wat gebeurt er verder met die gegevens?',
+    answer:
+      'Helemaal niets. Als je de app niet meer gebruikt wissen we jouw gegevens na 3 maanden.',
+  },
+  {
+    question: 'Kan ik mijn gegevens wijzigen of verwijderen?',
+    answer:
+      'Ja, dat kan (maar soms ook niet). Klik op de link voor meer informatie: amsterdam.nl/privacy',
+  },
+  {
+    question: 'Wie kan ik bellen met vragen over mijn gegevens?',
+    answer:
+      'Bellen gatver ;). Je kunt wel mailen naar Robert Jan Taling, onze TADA inspirator: r.j.taling@amsterdam.nl.',
+  },
+  {
+    question: 'TADA? Wat is dat?',
+    answer:
+      'De gemeente Amsterdam past bij het gebruik van jouw persoonsgegevens 6 principes toe. Klik op de link om daar meer over te weten: tada.city',
+  },
+];
