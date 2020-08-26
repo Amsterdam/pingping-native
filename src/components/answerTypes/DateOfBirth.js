79482833c715302820224013e3fd25c5602c0136
Egemen <uzunaliegemen@gmail.com>
2020-08-26 13:21:03 +0200

    style the picker and add placeholders for it

--- src/components/answerTypes/DateOfBirth.js
+++ src/components/answerTypes/DateOfBirth.js
@@ -1,52 +1,60 @@
 import React from 'react';
-import {View, StyleSheet} from 'react-native';
+import {View, StyleSheet, Platform} from 'react-native';
 import {Picker} from '@react-native-community/picker';
 import {getDays, getMonths, getYears} from '../../utils/birthDayHelper';
 
 const styles = StyleSheet.create({
-  dayPicker: {
-    flex: 1,
-  },
-  picker: {flex: 2},
-  pickerContainer: {
+  container: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignSelf: 'stretch',
   },
+  pickerContainer: {flex: 1},
+  pickerAndroid: {
+    borderWidth: 1,
+    borderRadius: 5,
+    marginRight: 5,
+  },
 });
 
 const DateOfBirth = ({setState, state}) => {
+  const isIos = Platform.OS === 'ios';
   return (
-    <View style={styles.pickerContainer}>
-      <Picker
-        selectedValue={state.day}
-        style={styles.dayPicker}
-        onValueChange={(itemValue, itemIndex) =>
-          setState({...state, day: itemValue})
-        }>
-        <Picker.Item label="" value="" />
-        {getDays()}
-      </Picker>
-      <Picker
-        selectedValue={state.month}
-        style={styles.picker}
-        onValueChange={(itemValue, itemIndex) =>
-          setState({...state, month: itemValue})
-        }>
-        <Picker.Item label="" value="" />
-        {getMonths().map((month) => (
-          <Picker.Item label={month.label} value={month.value} key={month} />
-        ))}
-      </Picker>
-      <Picker
-        selectedValue={state.year}
-        style={styles.picker}
-        onValueChange={(itemValue, itemIndex) =>
-          setState({...state, year: itemValue})
-        }>
-        <Picker.Item label="" value="" />
-        {getYears()}
-      </Picker>
+    <View style={styles.container}>
+      <View style={[styles.pickerContainer, !isIos && styles.pickerAndroid]}>
+        <Picker
+          selectedValue={state.day}
+          onValueChange={(itemValue, itemIndex) =>
+            setState({...state, day: itemValue})
+          }>
+          <Picker.Item label="Dag" value="" />
+          {getDays()}
+        </Picker>
+      </View>
+
+      <View style={[styles.pickerContainer, !isIos && styles.pickerAndroid]}>
+        <Picker
+          selectedValue={state.month}
+          onValueChange={(itemValue, itemIndex) =>
+            setState({...state, month: itemValue})
+          }>
+          <Picker.Item label="maand" value="" />
+          {getMonths().map((month) => (
+            <Picker.Item label={month.label} value={month.value} key={month} />
+          ))}
+        </Picker>
+      </View>
+
+      <View style={[styles.pickerContainer, !isIos && styles.pickerAndroid]}>
+        <Picker
+          selectedValue={state.year}
+          onValueChange={(itemValue, itemIndex) =>
+            setState({...state, year: itemValue})
+          }>
+          <Picker.Item label="jaar" value="" />
+          {getYears()}
+        </Picker>
+      </View>
     </View>
   );
 };
