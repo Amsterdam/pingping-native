9e5e979ad916e2129aebf70f33200f40deb2327b
Egemen <uzunaliegemen@gmail.com>
2020-08-27 19:02:30 +0200

    splashscreen android

--- android/app/src/main/java/com/pingpingnative/MainActivity.java
+++ android/app/src/main/java/com/pingpingnative/MainActivity.java
@@ -1,8 +1,16 @@
 package com.pingpingnative;
 
 import com.facebook.react.ReactActivity;
+import org.devio.rn.splashscreen.SplashScreen; // Import this.
+import android.os.Bundle; // Import this.
 
 public class MainActivity extends ReactActivity {
+  // Add this method.
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    SplashScreen.show(this);
+    super.onCreate(savedInstanceState);
+  }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
