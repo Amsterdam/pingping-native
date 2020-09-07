e61782191633d863fe5c9362b779f6f33400a8b8
Egemen <uzunaliegemen@gmail.com>
2020-09-07 13:57:31 +0200

    small refactor

--- src/utils/authUtils.js
+++ src/utils/authUtils.js
@@ -40,25 +40,26 @@ export default async function userStatus(
   setOnboarder,
 ) {
   const token = await AsyncStorage.getItem('@access_token'); // GET ACCESS TOKEN
+
   if (!data && error) {
     console.log('My token is invalid or not linked to user');
     setOnboarder(true);
     return await doRegisterDevice(registerDevice); // IF I HAVE A TOKEN BUT IT IS NOT CORRECT, GET A NEW ONE
   }
+
   if (!token && !loading) {
     console.log('GETTING TOKEN');
     return await doRegisterDevice(registerDevice); // REGISTER DEVICE IF THERE IS NO TOKEN
   }
+
   if (data && data.getStatus && !error && !loading) {
     if (
       data.getStatus.currentTask ||
       data.getStatus.device.notificationStatus === 'Initial'
     ) {
       console.log('I AM LOGGED IN AND NEED TO GO TO ONBOARDING');
-      setOnboarder(true);
-      return; // IF I AM AUTHENTICATED AND HAVE ONBOARDING TASKS OPEN, KEEP ME IN THE ONBOARDING
+      return setOnboarder(true); // IF I AM AUTHENTICATED AND HAVE ONBOARDING TASKS OPEN, KEEP ME IN THE ONBOARDING
     }
-
     return setLogin(); // I HAVE A VALID ACCESS TOKEN AND AM AUTHORIZED AND I HAVE COMPLETED THE ONBOARDING
   }
 }
