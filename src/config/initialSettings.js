c0c3bb592df78053f54fb9e6deda95174e17c188
Egemen <uzunaliegemen@gmail.com>
2020-10-20 14:01:28 +0200

    WIP - Need to configure urls

--- src/config/initialSettings.js
+++ src/config/initialSettings.js
@@ -1,3 +1,7 @@
-// export const BASE_URL = 'http://192.168.1.110:4000';
-export const BASE_URL = 'https://acc.api.pingping.amsterdam.nl';
+// WIP - Need to configure this with env vars
+export const LOCAL_URL = 'http://192.168.1.110:4000';
+export const ACC_URL = 'https://acc.api.pingping.amsterdam.nl';
+export const PROD_URL = 'https://api.pingping.amsterdam.nl';
+
+export const BASE_URL = PROD_URL;
 export const API_URL = `${BASE_URL}/api`;
