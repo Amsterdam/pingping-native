22ea85a4c26babf84f66d5904c8c1ba90235a213
Egemen <uzunaliegemen@gmail.com>
2020-11-02 14:24:17 +0100

    update privacy policy

--- src/helpers/faqItems.js
+++ src/helpers/faqItems.js
@@ -3,7 +3,7 @@ export const faqItems = [
     question:
       'Welke gegevens van mij gebruiken jullie voor het advies over mijn route?',
     answer:
-      'Geboortemaand/jaar, adres, bankrekening, DigiD, Zorgverzekering, Zorgtoeslag, Inschrijving Woningnet.',
+      'Je geboortedatum, of je een vast adres hebt, of je een bankrekening hebt, of je een DigiD hebt, of je een Zorgverzekering hebt, of je Zorgtoeslag hebt, of je staat ingeschreven bij Woningnet. Dit slaan we op middels een code, die niet te herleiden is tot jou. ',
   },
   {
     question: 'Waarom slaan jullie mijn gegevens op?',
@@ -11,7 +11,7 @@ export const faqItems = [
       'Omdat dit makkelijker is voor jou om de app te gebruiken. Dan hoef je niet elke keer alles opnieuw in te vullen.',
   },
   {
-    question: 'Waar slaan jullie mijn gegevens op ?',
+    question: 'Waar slaan jullie mijn gegevens op?',
     answer:
       'We slaan jouw gegevens veilig op bij het datapunt van de gemeente Amsterdam. ',
   },
@@ -23,7 +23,7 @@ export const faqItems = [
   {
     question: 'Kan ik mijn gegevens wijzigen of verwijderen?',
     answer:
-      'Ja, dat kan (maar soms ook niet). Klik op de link voor meer informatie: amsterdam.nl/privacy',
+      'Ja, dat kan in de app Ping Ping, voor andere diensten kijk voor meer informatie op deze link: www.amsterdam.nl/privacy ',
   },
   {
     question: 'Wie kan ik bellen met vragen over mijn gegevens?',
@@ -33,6 +33,6 @@ export const faqItems = [
   {
     question: 'TADA? Wat is dat?',
     answer:
-      'De gemeente Amsterdam past bij het gebruik van jouw persoonsgegevens 6 principes toe. Klik op de link om daar meer over te weten: tada.city',
+      'De gemeente Amsterdam past bij het gebruik van jouw persoonsgegevens 6 principes toe. Klik op de link om daar meer over te weten: tada.city ',
   },
 ];
