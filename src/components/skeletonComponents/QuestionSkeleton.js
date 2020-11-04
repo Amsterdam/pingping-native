0d34fa8b8ce78bbac1a8ce80d97ec87211a1a4e7
Egemen <uzunaliegemen@gmail.com>
2020-11-04 17:40:31 +0100

    added a skeleton loading for questionscreen and for routedetails

---
+++ src/components/skeletonComponents/QuestionSkeleton.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+import SkeletonPlaceholder from 'react-native-skeleton-placeholder';
+
+const QuestionSkeleton = () => (
+  <SkeletonPlaceholder>
+    <View style={styles.container}>
+      <View>
+        <View style={styles.title} />
+        <View style={styles.subTitle} />
+      </View>
+      <View>
+        <View style={styles.button} />
+        <View style={styles.button} />
+      </View>
+      <View>
+        <View style={styles.nextButton} />
+      </View>
+    </View>
+  </SkeletonPlaceholder>
+);
+
+const styles = StyleSheet.create({
+  container: {flex: 1, justifyContent: 'space-between'},
+  questionContainer: {
+    justifyContent: 'center',
+  },
+  title: {width: '100%', height: 40, borderRadius: 4, marginTop: 10},
+  subTitle: {width: '80%', height: 40, marginTop: 6, borderRadius: 4},
+  button: {marginTop: 6, width: '100%', height: 50, borderRadius: 30},
+  nextButton: {
+    marginTop: 6,
+    width: 100,
+    height: 25,
+    borderRadius: 6,
+    alignSelf: 'flex-end',
+  },
+});
+export default QuestionSkeleton;
