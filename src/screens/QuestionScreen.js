693a93be0fcf364a3311dbfd4df5a6090615dfdd
Egemen <uzunaliegemen@gmail.com>
2020-11-26 16:55:56 +0100

    Clean ups

--- src/screens/QuestionScreen.js
+++ src/screens/QuestionScreen.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import {StyleSheet} from 'react-native';
 import {useMutation, useQuery} from '@apollo/client';
 import PropTypes from 'prop-types';
