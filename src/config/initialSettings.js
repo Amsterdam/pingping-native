e8b4942d7182a9b326b9ce8987e2977fbea7c226
Egemen <uzunaliegemen@gmail.com>
2021-01-13 15:00:22 +0100

    fixes detox tests and detox build runner

--- src/config/initialSettings.js
+++ src/config/initialSettings.js
@@ -1,7 +1,7 @@
-// WIP - Need to configure this with env vars
 // const LOCAL_URL = 'http://192.168.1.110:4000';
-// const ACC_URL = 'https://acc.api.pingping.amsterdam.nl';
+const ACC_URL = 'https://acc.api.pingping.amsterdam.nl';
 const PROD_URL = 'https://api.pingping.amsterdam.nl';
 
-export const BASE_URL = PROD_URL;
+export const BASE_URL = __DEV__ ? ACC_URL : PROD_URL;
+console.log(BASE_URL);
 export const API_URL = `${BASE_URL}/api`;
