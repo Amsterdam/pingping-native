f01bce6f53738370ad06d1b511a1593fb984d630
Egemen <uzunaliegemen@gmail.com>
2021-12-22 17:30:18 +0300

    v2 tabbar was removed migrated to react-native-tab-view

--- src/screens/CityPingsHomeScreen.js
+++ src/screens/CityPingsHomeScreen.js
@@ -3,7 +3,8 @@ import React from 'react';
 import {useLazyQuery} from '@apollo/client';
 import {Box} from 'native-base';
 import PropTypes from 'prop-types';
-import {StatusBar, StyleSheet, View} from 'react-native';
+import {StatusBar, StyleSheet, View, useWindowDimensions} from 'react-native';
+import {TabView, TabBar, SceneMap} from 'react-native-tab-view';
 
 import GET_AVAILABLE_REWARDS from '../apollo/Query/getAvailableRewards';
 import GET_STATUS_QUERY from '../apollo/Query/getStatusQuery';
@@ -24,6 +25,13 @@ const CityPingsHomeScreen = ({navigation}) => {
 		});
 		return unsubscribe;
 	}, [navigation, getAvailableRewards, getStatus]);
+	const layout = useWindowDimensions();
+
+	const [index, setIndex] = React.useState(0);
+	const [routes] = React.useState([
+		{key: 'rewards', title: 'Rewards'},
+		{key: 'geclaimed', title: 'Geclaimed'},
+	]);
 
 	const [getAvailableRewards, availableRewards] = useLazyQuery(
 		GET_AVAILABLE_REWARDS,
@@ -58,6 +66,45 @@ const CityPingsHomeScreen = ({navigation}) => {
 	const balance = me?.data?.getStatus?.user.balance; // maybe move this part to either localstate or the tabnavigator
 	const claimedRewards = me?.data?.getStatus?.user.rewards;
 
+	const FirstRoute = () => (
+		<AvailableRewardsList
+			availableRewards={availableRewards}
+			navigation={navigation}
+			balance={balance}
+			getStatus={getStatus}
+			onRefresh={onRefresh}
+			refreshing={refreshing}
+		/>
+	);
+
+	const SecondRoute = () => (
+		<ClaimedRewardsList
+			claimedRewards={claimedRewards}
+			navigation={navigation}
+			balance={balance}
+			getStatus={getStatus}
+			onRefresh={onRefresh}
+			refreshing={refreshing}
+		/>
+	);
+
+	const renderScene = SceneMap({
+		rewards: FirstRoute,
+		geclaimed: SecondRoute,
+	});
+	const renderTabBar = props => (
+		<TabBar
+			{...props}
+			indicatorStyle={{backgroundColor: appColors.white}}
+			style={{backgroundColor: appColors.primary}}
+			renderLabel={({route, focused, color}) => (
+				<Title variant="h5" style={{color: appColors.white}}>
+					{route.title}
+				</Title>
+			)}
+		/>
+	);
+
 	return (
 		<Container style={styles.container}>
 			<Box style={styles.header}>
@@ -72,30 +119,13 @@ const CityPingsHomeScreen = ({navigation}) => {
 					<CitypingsChip value={balance} />
 				</View>
 			</Box>
-			{/* <Tabs
-				tabBarUnderlineStyle={styles.tabBarUnderlineStyle}
-				tabContainerStyle={styles.shadowRemover}>
-				<Tab heading="Rewards" {...TAB_STYLE}>
-					<AvailableRewardsList
-						availableRewards={availableRewards}
-						navigation={navigation}
-						balance={balance}
-						getStatus={getStatus}
-						onRefresh={onRefresh}
-						refreshing={refreshing}
-					/>
-				</Tab>
-				<Tab heading="Geclaimed" {...TAB_STYLE}>
-					<ClaimedRewardsList
-						claimedRewards={claimedRewards}
-						navigation={navigation}
-						balance={balance}
-						getStatus={getStatus}
-						onRefresh={onRefresh}
-						refreshing={refreshing}
-					/>
-				</Tab>
-			</Tabs> */}
+			<TabView
+				navigationState={{index, routes}}
+				renderScene={renderScene}
+				renderTabBar={renderTabBar}
+				onIndexChange={setIndex}
+				initialLayout={{width: layout.width}}
+			/>
 		</Container>
 	);
 };
