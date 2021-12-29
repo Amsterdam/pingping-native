b1f083480f56d782024c82f826e7449ea6d8d4bb
Egemen <uzunaliegemen@gmail.com>
2021-12-29 11:47:35 +0100

    clean up and update bootsplash package and remove unused files

--- android/app/src/main/java/com/pingpingnative/MainApplication.java
+++ android/app/src/main/java/com/pingpingnative/MainApplication.java
@@ -11,8 +11,8 @@ import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 import com.wix.reactnativenotifications.RNNotificationsPackage;
-import com.facebook.react.bridge.JSIModulePackage; // <- add
-import com.swmansion.reanimated.ReanimatedJSIModulePackage; // <- add
+import com.facebook.react.bridge.JSIModulePackage;
+import com.swmansion.reanimated.ReanimatedJSIModulePackage;
 
 
 public class MainApplication extends Application implements ReactApplication {
