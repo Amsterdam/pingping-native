a55da3c00f2819942df0b4e0d36f3048401d47e4
Egemen <uzunaliegemen@gmail.com>
2022-01-04 12:16:33 +0100

    use the usenetinfo infohook

--- src/App/AppContext.js
+++ src/App/AppContext.js
@@ -6,7 +6,7 @@ import React, {
 } from 'react';
 
 import { useQuery } from '@apollo/client';
-import NetInfo from '@react-native-community/netinfo';
+import { useNetInfo } from '@react-native-community/netinfo';
 import PropTypes from 'prop-types';
 
 import GET_STATUS_QUERY from '../apollo/Query/getStatusQuery';
@@ -23,6 +23,7 @@ export const AppContext = createContext({
 export default function AppContextProvider({
 	children,
 }) {
+	const netInfo = useNetInfo();
 	const { refetch } = useQuery(GET_STATUS_QUERY, {
 		fetchPolicy: 'network-only',
 		skip: 'true',
@@ -35,29 +36,23 @@ export default function AppContextProvider({
 	const [bootIssue, setBootIssue] = useState('');
 
 	useEffect(() => {
-		NetInfo.addEventListener(netInfoState => {
-			// here we check if there is an internect connection
-			// if we have an internet connection we will move with executing functions
-			// otherwise we present the user with a no connections screen
-			if (
-				netInfoState.isInternetReachable === true
-			) {
-				userStatus(
-					refetch,
-					setUserState,
-					setBootIssue,
-				);
-			}
-			if (
-				netInfoState.isInternetReachable === false
-			) {
-				setBootIssue(ERROR_TYPES.networkError);
-			}
-		});
-	}, [refetch]);
+		// here we check if there is an internect connection
+		// if we have an internet connection we will move with executing functions
+		// otherwise we present the user with a no connections screen
+		if (netInfo.isInternetReachable === true) {
+			userStatus(
+				refetch,
+				setUserState,
+				setBootIssue,
+			);
+		}
+		if (netInfo.isInternetReachable === false) {
+			setBootIssue(ERROR_TYPES.networkError);
+		}
+	}, [netInfo.isInternetReachable, refetch]);
 
-	const retry = () => {
-		userStatus(
+	const retry = async () => {
+		await userStatus(
 			refetch,
 			setUserState,
 			setBootIssue,
