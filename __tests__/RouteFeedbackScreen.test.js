b2b214ef1f8a21db6689dcb26d285c9857e522a1
Egemen <uzunaliegemen@gmail.com>
2022-03-14 15:45:44 +0100

    Rename all tests and add another snapshot test

---
+++ __tests__/RouteFeedbackScreen.test.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import { MockedProvider } from '@apollo/client/testing';
+import { NativeBaseProvider } from 'native-base';
+import renderer from 'react-test-renderer';
+
+import RouteFeedbackScreen from '../src/screens/RouteFeedbackScreen';
+
+jest.useFakeTimers();
+const navigation = {
+	navigate: jest.fn(),
+	addListener: jest.fn(),
+};
+
+test('renders correctly', () => {
+	const tree = renderer
+		.create(
+			<NativeBaseProvider>
+				<MockedProvider mocks={[]}>
+					<RouteFeedbackScreen navigation={navigation} />
+				</MockedProvider>
+			</NativeBaseProvider>
+		)
+		.toJSON();
+	expect(tree).toMatchSnapshot();
+});
